9309cf12df0c36bf82aafc6b6390971b8a6ed49e
LaRenzo <dlarenzo@outlook.com>
2024-01-18 11:46:12 -0700

    finalized update

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const PORT = 3000;
-
-app.use("/", require("./routes"));
-
-app.listen(3000, () => {
-  console.log(`Test server running on port: ${PORT}`);
-});
